21afa3fdefbfade3d3bea228e899f2fc4493b60e
Carlos Chacón <carlos.echc11@gmail.com>
2023-10-10 14:18:12 +0000

    MAIN

---
+++ server/src/main.js
@@ -0,0 +1,23 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { router } from "./routes";
+import { errorHandlers } from "./middlewares/errorHandlers";
+import config from "./config";
+
+const app = new Hono();
+app.use("*", cors());
+app.route("/", router);
+app.use("*", (c, next) => errorHandlers(c, next))
+app.onError((err, c) => {
+    console.error("Global Aplication Error :>> ", err)
+    return c.text("App Error Message", 500);
+})
+
+console.log(
+    `App: ${config.APP_NAME} v${config.APP_VERSION} listening to port ${config.NODE_PORT} - BUN JS | REST 🚀`
+);
+
+export default {
+    fetch: app.fetch,
+    port: config.NODE_PORT || 3000
+}
